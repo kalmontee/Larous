a040de87dc36785d7fcd3cce907bcae930a3a234
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-11-18 18:02:40 -0500

    Styled the Home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,20 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import SearchInput from "../components/SearchInput/SearchInput";
-import BeersCard from "../components/Beers/Beers";
+import BeersCard from "../components/Beers/BeersCard";
 import axios from "axios";
+import Skeleton from "@material-ui/lab/Skeleton";
+
+const classes = {
+  BeerSection: {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gap: "0px",
+    gridAutoRows: "minmax(100px, auto)",
+    margin: "0 50px",
+  },
+};
 
 const Home = () => {
   const [value, setValue] = useState("");
@@ -11,36 +23,36 @@ const Home = () => {
   useEffect(() => {
     axios
       .get(`https://api.punkapi.com/v2/beers`)
-      .then((res) => setBeers(res.data))
+      .then((res) => {
+        setBeers(res.data);
+      })
       .catch((err) => console.log(err)); // pop an alert for error
   }, []);
-
-  // const loadData = async () => {
-  //   const response = await fetch("https://api.punkapi.com/v2/beers");
-  //   const data = await response.json();
-  //   setBeers(data);
-  // }
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div>
+    <article>
       <SearchInput
         name={value}
         formSubmit={(event) => formSubmitHandler(event)}
         changeHandler={(e) => setValue(e.target.value)}
       />
-      {beers.map((beer) => (
-        <BeersCard
-          key={beer.id}
-          img={beer.image_url}
-          title={beer.name}
-          description={beer.description}
-        />
-      ))}
-    </div>
+      <section style={classes.BeerSection}>
+        {beers.map((beer) => (
+          <Link key={beer.id} to={"/beer/" + beer.id}>
+            <BeersCard
+              key={beer.id}
+              img={beer.image_url}
+              title={beer.name}
+              description={beer.description}
+            />
+          </Link>
+        ))}
+      </section>
+    </article>
   );
 };
 
