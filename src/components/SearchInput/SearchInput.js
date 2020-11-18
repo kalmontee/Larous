db9d9ca4d30c6a34646375414ab55fb4e4e46239
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-11-17 20:14:03 -0500

    Designed the Input search  and implemented Material UI button.

---
+++ src/components/SearchInput/SearchInput.js
@@ -0,0 +1,25 @@
+// import { useState } from "react";
+
+import classes from "./SearchInput.module.css";
+import { Button } from "@material-ui/core";
+
+const SearchInput = (props) => {
+  return (
+    <form className={classes.FormControl} onSubmit={props.formSubmit}>
+      <input
+        className={classes.Input}
+        placeholder="Search for beers.."
+        type="search"
+        name={props.name}
+        onChange={props.changeHandler}
+      />
+
+      {/* Material UI Button */}
+      <Button type="submit" size="large" variant="contained" color="primary">
+        Search
+      </Button>
+    </form>
+  );
+};
+
+export default SearchInput;
