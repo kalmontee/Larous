d3f938632ef44bdbbc11503f1fa759afe699c1b0
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-11-18 18:03:23 -0500

    Modified Beers component. Removed Material UI components and created my own card.

---
+++ src/components/Beers/Beers.js
@@ -0,0 +1,26 @@
+import Button from "@material-ui/core/Button";
+import classes from "./BeersCard.module.css";
+
+const BeersCard = (props) => {
+  return (
+    <div className={classes.BeersCard}>
+      <div className={classes.Container}>
+        <aside className={classes.aside}>
+          <img src={props.img} alt={props.name} className={classes.image} />
+          <div className={classes.Description}>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </div>
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+export default BeersCard;
