75f4773598f3d7ca4877bc43cffb8a368ad3cbc3
Kelvin Almonte <kelvin.almonte95@gmail.com>
2020-11-21 22:12:22 -0500

    Modified App.js adding the new NotFound page component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavigationItems from "./components/Navigation/NavigationItems";
 import Home from "./pages/Home";
 import Details from './pages/Details';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/beer/:id" component={Details} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
